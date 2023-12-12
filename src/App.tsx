d97c13fc49f00a896e7b6abd4c0c42f3a1554b11
adnane <adnane@pop-os.localdomain>
2023-12-12 12:55:18 +0100

    resizable and draggable boxes

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import Box from './Box';
+
+interface Axis {
+  x: number;
+  y: number;
+}
+
+interface Size {
+  width: number;
+  height: number;
+}
+
+interface BoxProps {
+  id: number;
+  position: Axis;
+  size: Size;
+  onDrag: (index: number, d: { x: number; y: number }) => void;
+  onResize: (index: number, size: Size) => void;
+}
+
+const App = () => {
+  const initialBoxes: BoxProps[] = [];
+
+  let box: BoxProps;
+  let nextX = 10;
+
+for (let id: number = 0; id < 4; id++) {
+  const storedPosition = JSON.parse(sessionStorage.getItem(`box-${id}-position`) || `{"x": ${nextX}, "y": 10}`);
+  const storedSize = JSON.parse(sessionStorage.getItem(`box-${id}-size`) || '{"width": 200, "height": 200}');
+
+  box = { id, position: storedPosition, size: storedSize, onDrag: () => {}, onResize: () => {} };
+  initialBoxes.push(box);
+
+  nextX = storedPosition.x + storedSize.width + 10;
+}
+
+  const [boxes, setBoxes] = useState<BoxProps[]>(initialBoxes);
+
+  useEffect(() => {
+    // Save the updated values in session storage whenever boxes change
+    boxes.forEach((box) => {
+      sessionStorage.setItem(`box-${box.id}-position`, JSON.stringify(box.position));
+      sessionStorage.setItem(`box-${box.id}-size`, JSON.stringify(box.size));
+    });
+  }, [boxes]);
+
+  const handleBoxDragStop = (index: number, d: { x: number; y: number }) => {
+    setBoxes((prevBoxes) => {
+      const updatedBoxes = [...prevBoxes];
+      updatedBoxes[index].position = { x: d.x, y: d.y };
+      return updatedBoxes;
+    });
+  };
+
+  const handleBoxResize = (index: number, newSize: Size) => {
+    setBoxes((prevBoxes) => {
+      const updatedBoxes = [...prevBoxes];
+      updatedBoxes[index].size = newSize;
+      return updatedBoxes;
+    });
+  };
+
+  return (
+    <div className='h-screen w-screen bg-white'>
+      {boxes.map((box, index) => (
+        <Box
+          key={box.id}
+          id={box.id}
+          position={box.position}
+          size={box.size}
+          onDrag={(d) => handleBoxDragStop(index, d)}
+          onResize={(s) => handleBoxResize(index, s)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Box.tsx
@@ -0,0 +1,45 @@
+// Box.tsx
+import React, { useState } from 'react';
+import { Rnd } from 'react-rnd';
+
+interface Axis {
+  x: number;
+  y: number;
+}
+
+interface Size {
+  width: number;
+  height: number;
+}
+
+interface BoxProps {
+  id: number;
+  position: Axis;
+  size: Size; 
+  onDrag: (d: { x: number; y: number }) => void;
+  onResize: (size: Size) => void;
+}
+
+const Box: React.FC<BoxProps> = ({ id, position, size, onDrag, onResize }: BoxProps) => {
+
+  return (
+    <Rnd
+      className='bg-gray-900'
+      size={{ width: size.width, height: size.height }}
+      position={{ x: position.x, y: position.y }}
+      onDragStop={(e, d) => {
+        onDrag({ x: d.x, y: d.y });
+      }}
+      bounds={'parent'}
+      onResize={(e, direction, ref, delta, position) => {
+          onDrag({ x: position.x, y: position.y })
+          onResize({ width: ref.offsetWidth, height: ref.offsetHeight });
+      }}
+    >
+      <span className="text-white">{`Box ${id}`}</span>
+    </Rnd>
+      // </div>
+  );
+};
+
+export default Box;
